1ff6baa0b4821bf9a484ff204f647d6e2cbeba12
shaobi-n <shaobin@outlook.sg>
2022-02-26 13:58:00 +0800

    basic pseudocode

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-var main = function (input) {
-  var myOutputValue = 'hello world';
-  return myOutputValue;
-};
+// There will be only two players. One human and one computer (for the Base solution).
+// The computer will always be the dealer.
+// Each player gets dealt two cards to start.
+// If a blackjack is found, a player will instant win.
+// The player goes first, and decides if they want to hit (draw a card) or stand (end their turn).
+// The dealer has to hit if their hand is below 17.
+// Each players' score is the total of their card ranks. Jacks/Queen/Kings are 10. Aces can be 1 or 11.
+// The player who is closer to, but not above 21 wins the hand.
